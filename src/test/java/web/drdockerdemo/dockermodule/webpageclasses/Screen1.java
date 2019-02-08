29195382badb3c4a79a69310baea7b93045147ed
santhosh-annam <santhosh.annam@devrabbit.com>
2019-02-08 12:46:32 +0530

    helloWorld

--- src/test/java/web/drdockerdemo/dockermodule/webpageclasses/Screen1.java
+++ src/test/java/web/drdockerdemo/dockermodule/webpageclasses/Screen1.java
@@ -9,7 +9,7 @@ import com.relevantcodes.extentreports.ExtentTest;
 @SuppressWarnings("unused")
 public class Screen1 extends BaseClass {
 	
-	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"http://devrabbit.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/UploadReportFile\",\"project_name\":\"DRDockerDemo\",\"project_description\":\"desc\",\"project_id\":224,\"module_name\":\"DockerModule\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":362,\"testcase_name\":\"TC1\",\"testcase_id\":433,\"testset_id\":0,\"executed_timestamp\":-874348820,\"browser_type\":\"chrome\"}";
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"http://devrabbit.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/UploadReportFile\",\"project_name\":\"DRDockerDemo\",\"project_description\":\"desc\",\"project_id\":224,\"module_name\":\"DockerModule\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":362,\"testcase_name\":\"TC1\",\"testcase_id\":433,\"testset_id\":0,\"executed_timestamp\":-873004582,\"browser_type\":\"chrome\"}";
 
 	public static String projectName = "drdockerdemo";
 	public WebDriver driver;
